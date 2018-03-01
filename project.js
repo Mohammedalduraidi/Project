b11e2cd418c6d123d9bb928191972135083b4f03
Mohammedalduraidi <mohd.alduraidi@gmail.comgit config --global user.email git config --global user.email mohd.alduraidi@gmail.com>
2018-03-01 13:59:50 +0200

    first update

--- project.js
+++ project.js
@@ -1,81 +1,73 @@
-
-// function each (array, func){
-//     for (var i = 0; i < array.length; i++) {
-//         func(array[i], i)
-//     }
-// }
-var array12=["drew"];
-var x=0
 $(document).ready(function(){
-$(array).hide()
-$(array[0]).show(300)
-
-$('#btn1').on('click',function(){
-	
-
-    ++x;
-    if(x===array.length){
-
-         x=0
-     }
-    for (var i = 0; i < array.length; i++) {
-         $(array[i]).hide(300)    
-     }
-     $(array[x]).show(300)
+    $(array).hide()
+    $(array[0]).show(300)
+        //make a movement steps inside the array
+    $('#btn1').on('click',function(){
+        //this is for next btn
+        ++x;
+        if(x===array.length){
+            x=0
+        }
+        for (var i = 0; i < array.length; i++) { 
+             $(array[i]).hide(300)    
+        }
+        $(array[x]).show(300)
     })
-$('#btn2').on('click',function(){
-	$("#forall").show(300)
-    --x;
-    if(x===-1){
-
+    $('#btn2').on('click',function(){
+       //this is for preview btn	   
+        --x;
+       if(x===-1){
          x=array.length-1
-     }
-    for (var i = 0; i < array.length; i++) {
+       }
+       for (var i = 0; i < array.length; i++) {
          $(array[i]).hide(300)    
-     }
-     $(array[x]).show(300) 
-
-
-    
-})
-$(".user").focus(function(){
-	$(this).css('background',"lightblue")
-
-})
-$(".user").blur(function(){
-	$(this).css('background',"white")
-
-})
-$('#refresh').click(function() {
-    location.reload();
+       }
+       $(array[x]).show(300) 
+    })
+    //this is for add some effect to the input
+    $("#com").focus(function(){
+        $(this).css('background',"lightblue")
+    })
+    $("#com").blur(function(){
+        $(this).css('background',"white")
+    })
+    $(".firstinput").focus(function(){
+        $(this).css('background',"lightblue")
+    })
+    $(".firstinput").blur(function(){
+        $(this).css('background',"white")
+    })
+    //this is for make refresh to the page
+    $('#refresh').click(function() {
+        location.reload();
+    });
+    //this function for add the comments
+    $('#com').on('click',function(){
+        // var array12=  JSON.parse(localStorage.getItem("Array12"))
+        // var str = $("#co").val();
+        // array12.push(str);
+        // for (var i = 0; i < array12.length; i++) {    
+        var para = $('<p></p>')
+        para.attr("id","forcomment");
+        $('body').append(para)
+        para.text($("#co").val()) 
+        // localStorage.setItem("Array12", JSON.stringify(array12));
+    })
+    // to let the user add subject
+    $("#submitit").on('click',function(){
+        var newdiv = $('<div id="newmeta"></div>')
+        $('#forall').append(newdiv)
+        var newpara = $('<p></p>')
+        newpara.text($('#imgtext').val())
+        var newdiv2 = $('<div class = "col-sm-4 img1" id = "image123"></div>')
+        var newimage = $('<img class = "img1">')
+        var any11 = $("#imglink").val()
+        newimage.attr("src", any11)
+        $('#image123').append(newimage)   
+        $('#newmeta').append(newdiv2)
+        $('#newmeta').append(newpara)
+        array.push(document.getElementById("newmeta"))
+    })
 });
-var count =0;
-$('#com').on('click',function(){
-   // var array12=  JSON.parse(localStorage.getItem("Array12"))
-// var str = $("#co").val();
-// array12.push(str);
-// for (var i = 0; i < array12.length; i++) {
-      count++;
-   var para = $('<p></p>')
-   para.attr("id","forcomment"+count);
- $('body').append(para)
-para.text($("#co").val()) 
-
-// localStorage.setItem("Array12", JSON.stringify(array12));
-
-
-})
-$("#com").focus(function(){
-    $(this).css('background',"lightblue")
-
-})
-$("#com").blur(function(){
-    $(this).css('background',"white")
-
-})
-
-});
- 
-
-
+var x=0
 var array = [document.getElementById("paraandimg1"),document.getElementById("paraandimg2"),document.getElementById("paraandimg3")]
